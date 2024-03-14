897305e265b689c191bc5974007580d4bebc028d
Rajeshkumarachari <131753283+Rajeshkumarachari@users.noreply.github.com>
2024-03-14 10:23:36 +0530

    home feed created

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
+import Feed from "./components/Feed";
 import Sidebar from "./components/Sidebar";
 
 export default function Home() {
   return (
     <main className=" min-h-screen flex max-w-7xl mx-auto ">
-      {/* {Sidebar} */}
       <Sidebar />
       {/* { feed} */}
+      <Feed />
+
       {/* { Widgets} */}
     </main>
   );
