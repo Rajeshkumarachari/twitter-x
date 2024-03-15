7c45370f5a01e3d2bb2675faea3ede41797a9a21
Rajeshkumarachari <131753283+Rajeshkumarachari@users.noreply.github.com>
2024-03-15 10:22:08 +0530

    follow more section created

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,12 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     //domain: ["images.unsplash.com"],
-    domains: ["www.cnbctv18.com", "www.moneycontrol.com","auto.hindustantimes.com"],
+    domains: [
+      "www.cnbctv18.com",
+      "www.moneycontrol.com",
+      "auto.hindustantimes.com",
+      "randomuser.me",
+    ],
   },
 };
 
--- src/app/components/Widgets.js
+++ src/app/components/Widgets.js
@@ -3,11 +3,14 @@
 import { SearchIcon } from "@heroicons/react/outline";
 import { useEffect, useState } from "react";
 import News from "./News";
+import Image from "next/image";
 
 export default function Widgets() {
   const [articleNum, setArticleNum] = useState(3);
   const [newsData, setNewsData] = useState("");
-  console.log(newsData);
+  const [followData, setFollowData] = useState("");
+  const [followMoreNum, setFollowMoreNum] = useState(3);
+  console.log(followData);
 
   useEffect(() => {
     fetch("https://saurav.tech/NewsAPI/top-headlines/category/business/in.json")
@@ -16,7 +19,17 @@ export default function Widgets() {
       })
       .then((data) => {
         console.log(data);
-        setNewsData(data.articles);
+        setNewsData(data?.articles);
+      });
+  }, []);
+  useEffect(() => {
+    fetch("https://randomuser.me/api/?results=30&inc=name,login,picture")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data);
+        setFollowData(data?.results);
       });
   }, []);
   return (
@@ -41,6 +54,41 @@ export default function Widgets() {
         <button
           onClick={() => setArticleNum(articleNum + 3)}
           className=" hover:underline text-blue-400  pl-4 pb-3 hover:text-blue-500"
+        >
+          Show more
+        </button>
+      </div>
+      <div className=" sticky top-16 text-gray-700 space-y-3 bg-gray-100 pt-2 rounded-md w-[90%] sm:xl:w-[75%] mt-3 ">
+        <h4 className=" font-bold text-xl px-4 "> Who to Follow</h4>
+        {followData &&
+          followData.slice(0, followMoreNum).map((user) => (
+            <div
+              className=" flex items-center  px-4 py-2 cursor-pointer hover:bg-sky-100 rounded-lg"
+              key={user.login.username}
+            >
+              <Image
+                src={user?.picture?.thumbnail}
+                alt="user images"
+                width={40}
+                height={40}
+                className=" rounded-full"
+              />
+              <div className=" truncate ml-3 leading-5">
+                <h4 className=" font-semibold hover:underline text-[14px] truncate ">
+                  {user?.login.username}{" "}
+                </h4>
+                <h5 className=" text-[13px] text-gray-500 truncate ">
+                  {user?.name?.first + " " + user?.name?.last}
+                </h5>
+              </div>
+              <button className=" ml-auto bg-black text-white rounded-3xl text-sm px-3.5 py-1.5 font-bold  ">
+                Follow
+              </button>
+            </div>
+          ))}
+        <button
+          onClick={() => setFollowMoreNum(followMoreNum + 3)}
+          className="hover:underline text-blue-400  pl-4 pb-3 hover:text-blue-500"
         >
           Show more
         </button>
